45b52e5bcfed47203185b6d10c29e1caa060c776
kenyawest <judegantony@gmail.com>
2018-01-27 20:13:47 -0500

    added gradle /w simpletest file

---
+++ lab02/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -0,0 +1,6 @@
+class SimpleTest{  
+    public static void main(String args[]){  
+		System.out.println("Do you know de wei?!?");
+		System.out.print("Let me show you de wei of de devil!");
+    }  
+}
