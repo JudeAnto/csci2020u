0a583007875b9ae949c0c974342d1f186de3e6ff
kenyawest <judegantony@gmail.com>
2018-03-06 22:54:39 -0500

    raw spamFilter

--- assignment1/src/csci2020Assign.java
+++ assignment1/src/csci2020Assign.java
@@ -7,25 +7,31 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class SpamFilter {
 	
-	private static final String FILENAME = "C:\\Jude\\assignment1_data\\data\\train";
+	private static final String FILENAME = "C:\\Jude\\assignment1_data\\data";
 	//private static String hamF = "ham", hamF2 = "ham2", spamF = "spam";
 	public static Map<String, Float> mapWC = new HashMap<String, Float> ();
 	public static Map<String, Float> testHamFreq = new HashMap<String, Float> ();
 	public static Map<String, Float> testSpamFreq = new HashMap<String, Float> ();
+	public static List<TestFile> file = new ArrayList<TestFile>();
+	public static float spamValue = 0.0f;
 
-	
+	public static void testDir() {
+		//directoryReader("\\test\\ham");
+		directoryReader("\\test\\ham");
+	}
 	
 	public static void trainDir() {
-		directoryReader("\\ham");
-		directoryReader("\\ham2");
+		directoryReader("\\train\\ham");
+		directoryReader("\\train\\ham2");
 		testHamFreq = mapWC;
 		
 		mapWC = new HashMap<String, Float> ();
-		directoryReader("\\spam");
+		directoryReader("\\train\\spam");
 		testSpamFreq = mapWC;
 		//Map<String, String> mapWC = new HashMap<String, String> ();
 		
@@ -62,19 +68,22 @@ public class SpamFilter {
 	
 		for (File file : listOfFiles) {
 		    if (file.isFile()) {
-		    	//System.out.println(file.getName());
-		        bufferReader(file.getName(), keyW);
+		    	bufferReader(file.getName(), keyW);
 		    }
 		}
 	}
 	
+	
 	public static void bufferReader(String fileName, String keyW) {
+
     	try (BufferedReader br = new BufferedReader(new FileReader(FILENAME 
-    			+  "\\" + keyW +  "\\" + fileName))) {
+    			 + keyW +  "\\" + fileName))) {
     		
 			String sCurrentLine;
 			Boolean beginCount = false;
 			ArrayList<String> wordDup = new ArrayList<String>(); //word occurence in one file
+			float calculateN = 0.0f;
+			String fileType = "";
 			
 			while ((sCurrentLine = br.readLine()) != null) {
 				String[] words = sCurrentLine.split("\\W+");
@@ -84,22 +93,39 @@ public class SpamFilter {
 					for (String word: words) {
 					   word = word.toLowerCase();
 					   if ((word.length() == 1) || word.matches("-?\\d+")) {isJunk = true;}
-
-					   if (!(isJunk) && !mapWC.containsKey(word)) {
-						   mapWC.put(word, 1.0f);
-						   wordDup.add(word);
-					   } else if (!(isJunk) && !wordDup.contains(word)) {
-					        float count = mapWC.get(word);
-					        mapWC.put(word, count + 1);
-					        wordDup.add(word);
-				       }
+						 if (!keyW.equals("\\test\\ham") && !keyW.equals("\\test\\spam")) {
+						   if (!(isJunk) && !mapWC.containsKey(word)) {
+							   mapWC.put(word, 1.0f);
+							   wordDup.add(word);
+						   } else if (!(isJunk) && !wordDup.contains(word)) {
+						        float count = mapWC.get(word);
+						        mapWC.put(word, count + 1);
+						        wordDup.add(word);
+					       }
+						} else {
+							if (!wordDup.contains(word) && mapWC.get(word) != null) {
+								float tempVal = (float) (Math.log(1- mapWC.get(word)) - Math.log(mapWC.get(word)));
+								calculateN += tempVal;
+								wordDup.add(word);
+							}
+						}
 					}
+					
 				}
 				if(Arrays.asList(words).contains("Date")) {
 					beginCount = true;
 				}
 			}
-			//System.out.println(mapWC);
+			if (keyW.equals("\\test\\ham")) {
+				fileType = "Ham";
+			} else if (keyW.equals("\\test\\spam")) {
+				fileType = "Spam";
+			}
+			if (fileType.equals("Ham") || fileType.equals("Spam")) {
+				spamValue = (float) (1/(1+ Math.pow(Math.E, calculateN)));
+				TestFile newfile = new TestFile(fileName, (double)spamValue, fileType);
+				file.add(newfile);
+			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -111,7 +137,10 @@ public class SpamFilter {
 	
     public static void main(String args[]) {
     	trainDir();
-    	//System.out.println(mapWC);
+    	testDir();
+    	for (TestFile nfile: file) {
+    		System.out.println(nfile.getFilename());
+    	}
+    	//
 	}
 
-}
